b368860b334cbd738dd0c830d453b8bfd1062dc2
Faisal <officialfaisal1112@gmail.com>
2023-09-03 23:22:18 +0600

    remove dummy comments

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -8,13 +8,6 @@ import {
   usePostCommentMutation,
 } from '@/redux/api/apiSlice';
 
-const dummyComments = [
-  'Bhalo na',
-  'Ki shob ghori egula??',
-  'Eta kono product holo ??',
-  '200 taka dibo, hobe ??',
-];
-
 interface IProps {
   id: string;
 }
